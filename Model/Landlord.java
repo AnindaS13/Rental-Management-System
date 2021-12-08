79c76929bd5d82a55edd95fa09e5280864d2bc38
Lubaba Sheikh <lubaba.sheikh01@gmail.com>
2021-12-07 19:58:01 -0700

    fixed models

--- Model/Landlord.java
+++ Model/Landlord.java
@@ -1,7 +1,30 @@
 package Model;
 
-public class Landlord {
+import java.util.ArrayList;
 
+public class Landlord extends User {
+
+    // inherits email from user, use email as fk
+
+    private int messageID; // pk, nn, ai
+    private ArrayList<String> message; 
+
+    public Landlord()
+    {
+        super();
+    }
+
+    public Landlord(String email, String password, String fname, String lname, String role, int id, String message)
+    {
+        super(email, password, fname, lname, role);
+        this.messageID = id;
+        this.message.add(message);
+    }
+
+    public void addMessage (String m)
+    {
+        this.message.add(m);        
+    }
 
     // public Listing addProperty()
     // {
